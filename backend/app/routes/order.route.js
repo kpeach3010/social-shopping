0a5817ba65eae83c16d6d61f7704dc8868d21ac2
kpeach3010 <kimkhanh6933@gmail.com>
2025-12-22 19:21:12 +0700

    fix: chuyển router động xuống cuối file

--- backend/app/routes/order.route.js
+++ backend/app/routes/order.route.js
@@ -1,56 +1,63 @@
 import express from "express";
 import multer from "multer";
-const upload = multer();
 import * as OrderController from "../controllers/order.controller.js";
 import { authenticate, hasRoles } from "../middlewares/auth.middleware.js";
 import { Role } from "../enums/role.enum.js";
 
 const router = express.Router();
 
-router.get("/:id", OrderController.getOrderWithUserInfoByIdController);
-router.use(authenticate);
 router.post(
   "/checkout",
+  authenticate,
   hasRoles(Role.CUSTOMER),
   OrderController.checkoutController
 );
 router.patch(
   "/cancel/:id",
+  authenticate,
   hasRoles(Role.CUSTOMER),
   OrderController.cancelOrderController
 );
 router.get(
   "/my-orders",
+  authenticate,
   hasRoles(Role.CUSTOMER),
   OrderController.getMyOrdersController
 );
 router.get(
   "/my-orders/:id",
+  authenticate,
   hasRoles(Role.CUSTOMER),
   OrderController.getMyOrderByIdController
 );
 
 router.get(
   "/search",
+  authenticate,
   hasRoles(Role.STAFF),
   OrderController.searchOrdersByIdController
 );
 
 router.get(
   "/overview",
+  authenticate,
   hasRoles(Role.STAFF),
   OrderController.getOrdersOverviewForStaffController
 );
 
 router.patch(
   "/approve/:id",
+  authenticate,
   hasRoles(Role.STAFF),
   OrderController.approveOrderController
 );
 router.patch(
   "/reject/:id",
+  authenticate,
   hasRoles(Role.STAFF),
   OrderController.rejectOrderController
 );
+
+router.get("/:id", OrderController.getOrderWithUserInfoByIdController);
 
 export default router;

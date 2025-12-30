a77e95f346ae2a6b517db9e8a25667646856e1a8
kpeach3010 <kimkhanh6933@gmail.com>
2025-12-30 19:01:36 +0700

    fix(ui): thay đổi phông chữ trang đăng nhập

--- frontend/pages/login-page.vue
+++ frontend/pages/login-page.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8"
+    class="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8 font-sans"
   >
     <div class="sm:mx-auto sm:w-full sm:max-w-md">
       <h2 class="text-center text-3xl font-extrabold text-gray-900">
@@ -9,7 +9,7 @@
     </div>
 
     <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-      <div class="bg-white py-8 px-4 shadow-xl sm:rounded-lg sm:px-10">
+      <div class="bg-white py-8 px-4 shadow-xl sm:rounded-lg sm:px-10 font-sans">
         <form class="space-y-6" @submit.prevent="onSubmit">
           <!-- Email -->
           <div>
